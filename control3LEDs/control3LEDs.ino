414d72e6b47dbc6877e95645cf3ef68b32c9a63d
Jason Juhyun Kim <mtinet@hanmail.net>
2018-06-24 11:06:08 +0900

    Create control3LEDs.ino

---
+++ control3LEDs/control3LEDs.ino
@@ -0,0 +1,35 @@
+ char val; // Data received from the serial port
+ 
+int ledRed = 8; // Set the pin to digital I/O 4
+int ledYellow = 9; 
+int ledGreen = 10;
+
+void setup() {
+pinMode(ledRed, OUTPUT); // Set pin as OUTPUT
+pinMode(ledYellow, OUTPUT);
+pinMode(ledGreen, OUTPUT);
+Serial.begin(9600); // Start serial communication at 9600 bps
+}
+
+void loop() {
+  while (Serial.available()) { // If data is available to read,
+    val = Serial.read(); // read it and store it in val
+  }
+  if (val == 'a') { 
+   digitalWrite(ledRed, HIGH);
+   digitalWrite(ledYellow, LOW);
+   digitalWrite(ledGreen, LOW);
+  } else if (val == 'b') {
+   digitalWrite(ledRed, LOW);
+   digitalWrite(ledYellow, HIGH);
+   digitalWrite(ledGreen, LOW);
+  } else if (val == 'c') {
+   digitalWrite(ledRed, LOW);
+   digitalWrite(ledYellow, LOW);
+   digitalWrite(ledGreen, HIGH);
+  } else if (val == 'd') {
+   digitalWrite(ledRed, LOW);
+   digitalWrite(ledYellow, LOW);
+   digitalWrite(ledGreen, LOW);
+  } 
+}
